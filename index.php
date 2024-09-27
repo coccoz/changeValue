10ec41e5238f6ea5b8bd1c5beeecc4b6e8220d5b
cosimo <ballerini.co@gmail.com>
2024-09-27 07:45:04 +0200

    finito

--- index.php
+++ index.php
@@ -7,10 +7,15 @@
 </head>
 <body>
     <?php
-        
+        $r = rand(0,255);
+        $g = rand(0,255);
+        $b = rand(0,255);
+        $nome = "gianni";
+        $color = "rgb($r, $g, $b)";
 
+        echo"<h1 style='color: $color'>$nome</h1>";
 
-        
     ?>
+
 </body>
 </html>
