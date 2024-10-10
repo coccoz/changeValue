0097f6cd1974e6d4b69e51ef94d4d4bcfc06a0ec
cosimo <ballerini.co@gmail.com>
2024-10-10 08:51:33 +0200

    finito

--- index.php
+++ index.php
@@ -12,7 +12,16 @@
         $domanda = "Qual è il pianeta più vicino al sole?";
         $risposte = ["Venere", "Terra", "Marte", "Mercurio", "Giove"];
 
-        
+        echo("<p><b><i>$domanda</p></b></i>");
+
+        echo("<br>");
+
+        for ($i=0; $i < sizeof($risposte); $i++) { 
+            echo("<input type='radio' name='risposte' id='pianeti' value='risposta'/>
+            <label for='pianeti'>$risposte[$i]</label> <br>"
+        );
+            
+        }
 
 
     ?>
